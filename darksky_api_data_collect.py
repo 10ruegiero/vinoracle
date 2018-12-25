f2259fbca8cfc1f97ca4f8cf6432b72cf60e44fd
victor <victor@diruggiero.fr>
2018-12-25 20:47:25 +0100

    Vinoracle - Darksy API - Debugging dataFrame Generation avec special dict - Output OK

--- darksky_api_data_collect.py
+++ darksky_api_data_collect.py
@@ -66,11 +66,19 @@ def print_json(json):
     pp.pprint(type(json))
     pp.pprint(json)
 
+
+def init_dict(request,data_dict):
+    # Transforme un dictionnaire json, en un dictionnaire de listes
+    for key in request['daily']['data'][0]:
+        value = request['daily']['data'][0][key]
+        data_dict[key] = [value]
+    return data_dict
+
 def add_data(request,data_dict):
     # Transforme un dictionnaire json, en un dictionnaire de listes
     for key in request['daily']['data'][0]:
-        value = [request['daily']['data'][0][key]]
-        data_dict[key] = [value]
+        value = request['daily']['data'][0][key]
+        data_dict[key].append(value)
     return data_dict
 
 def main():
@@ -78,19 +86,24 @@ def main():
 
     # Date de début de la période et durée de la période
     date_init = time_stamp(2017, 6, 30)
-    duree = 30
+    duree = 5
     periode = time_table(date_init, duree)
 
     # Requêtes successives
+    count = 0
     for date_time_stamp in periode:
         request = api_get_request(date_time_stamp)
         # print_json(request)
-        add_data(request, data_dict)
+        if (count == 0):
+            init_dict(request,data_dict)
+        else:
+            add_data(request, data_dict)
+        count +=1
 
     print(data_dict)
 
     # Creating a pandas data frame
-    df = pandas.DataFrame(data=data_dict)
+    df = pandas.DataFrame(dict([(k,pandas.Series(v)) for k,v in data_dict.items()]))
     print(df)
     file_name = 'darksky_data_' + str(date_init) + '_' + str(duree) + '.csv'
     df.to_csv(file_name)
